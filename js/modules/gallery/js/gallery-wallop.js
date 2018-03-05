d91a6a50aa12efd8d5e45e9bed441b3c7b04699a
Juan 'Dientuki' Farias <dientuki@gmail.com>
2018-03-05 02:18:44 -0300

    added more files

---
+++ js/modules/gallery/js/gallery-wallop.js
@@ -0,0 +1,62 @@
+import Helper from '../../helpers/js/generic';
+//import Lazyload from '../../lazyload/js/lazyload';
+import Validators from '../../helpers/js/validators';
+import Wallop from '../vendor/Wallop';
+//import '../scss/wallop.scss';
+//import '../scss/wallop--slide.scss';
+
+export default class Gallery {
+
+  constructor(selectors, settings, dot) {
+    const defaults = { carousel: true },
+      valid = ['buttonPreviousClass', 'buttonNextClass', 'itemClass', 'currentItemClass', 'showPreviousClass',
+        'showNextClass', 'hidePreviousClass', 'hideNextClass', 'carousel', 'autoplay'];
+
+    if (typeof selectors !== 'undefined') {
+      if (typeof selectors !== 'string') {
+        throw new Error('"settings" is mandatory and must be a string.');
+      }
+    }
+
+    if (typeof settings !== 'undefined') {
+      if (typeof settings !== 'object') {
+        throw new Error('"settings" must be a json.');
+      }
+
+      Validators.settings(settings, valid);
+    }
+
+    this.element = document.querySelector(selectors);
+    this.dot = this.element.querySelector(dot)
+
+    if (this.element !== null) {
+      this.wallop = new Wallop(this.element, Helper.merge_objects(defaults, settings));
+
+      if (this.dot !== null) {
+        this.change();
+        this.click();
+      }
+    }
+  }
+
+  change(){
+    this.wallop.on('change', (event) => {
+      const dots = this.dot.querySelectorAll('.dot');
+
+      Array.from(dots).forEach((dot) => {
+        dot.classList.remove('selected');
+      });
+      dots[event.detail.currentItemIndex].classList.add('selected');
+
+    });
+  }
+
+  click(){
+    Array.from(this.dot.querySelectorAll('.dot')).forEach((dot, index) => {
+      dot.addEventListener('click', () => {
+        //console.log(this, index, a)
+        this.wallop.goTo(index)
+      });
+    });
+  }
+}
